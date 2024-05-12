af62c697a5b5b5b4a5349844135aa7b9de2fa812
smashera <anarkys0306@gmail.com>
2024-05-12 10:16:27 +0530

    Source COde

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/ccet");
+mongoose.connect("mongodb+srv://Tanya:Tanya@cluster0.5f3fdsp.mongodb.net/jalvidyut");
 
 const userSchema = mongoose.Schema({
   username: {
